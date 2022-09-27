1d479142ad896593595bc96fb7fc42535ec1efa7
Alex Delgado <alex.delgado@ias.com.co>
2022-09-26 19:48:17 -0500

    Se eliminan clases que no se utilizan.

--- src/main/java/com/bankinformationjobs/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-// package com.bankinformationjobs.repository;
-
-// import com.bankinformationjobs.domain.entity.Usuario;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
-
-// @Repository
-// public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-// }
